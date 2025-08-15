8bf1d7fc364ae77256ab460c5b25ee951f2e3349
ArGha-03 <arghabatabyal2003@gmail.com>
2025-08-15 23:07:32 +0530

    added prisma

--- apps/ws-backend/src/index.ts
+++ apps/ws-backend/src/index.ts
@@ -7,10 +7,11 @@ const wss = new WebSocketServer({port: 8080})
 wss.on('connection', (ws, request) => {
     const url = request.url;
     if(!url) return ws.close();
+    
     const queryParams = new URLSearchParams(url.split('?')[1]);
     const token = queryParams.get('token') ?? '';
-
     const decoded = jwt.verify(token, JWT_SECRET)
+
     if(!decoded) return ws.close();
 
     ws.on('message', (msg) => {
